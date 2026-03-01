61dc224c160b0b351b13dbf0e13e4a35eeeb5b1e
Rohith Subramanian Nithyadevi <rohithsn18@gmail.com>
2026-03-01 12:19:36 +0530

    Fixed duplicate useNavigate import

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,15 +8,15 @@ export default {
     extend: {
       colors: {
         brand: {
-          100: '#EDF2FA', // Lightest
+          100: '#EDF2FA', 
           200: '#D7E3FC',
           300: '#CCDBFD',
           400: '#C1D3FE',
-          500: '#ABC4FF', // Primary dark
+          500: '#ABC4FF', 
         }
       },
       fontFamily: {
-        sans: ['Outfit', 'sans-serif'], // Or 'Inter', 'Poppins', etc.
+        sans: ['Outfit', 'sans-serif'], 
       }
     },
   },
